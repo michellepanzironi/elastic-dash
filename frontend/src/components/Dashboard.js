25f72c32ed2b028a194ee9b8dbf496b0201a053e
Michelle Panzironi <michelle.panzironi@gmail.com>
2023-02-25 16:48:56 -0500

    api and client config

---
+++ frontend/src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dashboard = () => {
+	
+
+	console.log(`${process.env.REACT_APP_API_URL}/kibana_sample_data_logs/_search`);
+
+	return (
+		<div>
+			hello
+		</div>
+	)
+};
+
+export default Dashboard;
