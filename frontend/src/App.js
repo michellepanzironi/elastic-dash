df1846da216e466347a7d63844ed358646c338c7
Michelle Panzironi <michelle.panzironi@gmail.com>
2023-02-25 19:09:49 -0500

    add dash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
+  
+
   return (
     <div className="App">
-      hello
+      <Dashboard />
     </div>
   );
 }
--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,13 +1,17 @@
 import React from 'react';
+// import axios from 'axios';
+import { useLocalStorageState } from '../hooks/useLocalStorageState';
 
 const Dashboard = () => {
-	
+	const [data, setData] = useLocalStorageState('fetchData', {})
+	// axios.get()
+
 
 	console.log(`${process.env.REACT_APP_API_URL}/kibana_sample_data_logs/_search`);
 
 	return (
 		<div>
-			hello
+			hello charle
 		</div>
 	)
 };
